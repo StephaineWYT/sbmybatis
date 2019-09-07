6b6685d0e5f3ff60aad590f8e16471efc81cce1f
123 <820352756@qq.com>
2019-09-07 13:08:22 +0800

    third for page functions

--- src/main/java/wen/sbmybatis/model/Page.java
+++ src/main/java/wen/sbmybatis/model/Page.java
@@ -18,4 +18,36 @@ public class Page<E> implements Serializable {
         this.totalNumber = totalNumber;
         this.list = list;
     }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public long getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(long totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public long getTotalNumber() {
+        return totalNumber;
+    }
+
+    public void setTotalNumber(long totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public List<E> getList() {
+        return list;
+    }
+
+    public void setList(List<E> list) {
+        this.list = list;
+    }
 }
